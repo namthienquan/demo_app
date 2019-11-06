42a2680bf8a4ffc9dd6719a87104fc89b9ae6420
nam nguyen <nam.sondong@gmail.com>
2019-11-06 20:42:00 +0700

    sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/new'
+	root 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/home", to: "pages#home"
-  get "/help", to: "pages#help"
-  root "pages#home"
+  get  '/help',    to: 'pages#help'
+  get  '/about',   to: 'pages#about'
+  get  '/contact', to: 'pages#contact'
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
 end
